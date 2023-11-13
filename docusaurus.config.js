f6beb6e1ed6925acfab79109936583411d105388
Stoffel <86371768+vdwstoffel@users.noreply.github.com>
2023-11-13 23:59:11 +0100

    warn on broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   deploymentBranch: "main",
   trailingSlash: false,
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
