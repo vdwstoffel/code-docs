13ba21736c6b33660301ef6618bf082bb2dcf85c
Stoffel <86371768+vdwstoffel@users.noreply.github.com>
2024-08-01 20:27:23 +0200

    remove examples

--- examples/javascript/express/unit_testing_with_ts/src/routes/catRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { getCats, addCat } from "../controllers/catController";
-
-const router: Router = Router();
-
-router.route("/cat").get(getCats).post(addCat);
-
-export default router;
--- examples/javascript/react/redux_with_ts/src/api/auth.ts
+++
@@ -1,9 +0,0 @@
-import authSlice from "../store/authSlice";
-import { AppDispatch } from "../store/store";
-
-export const signInUser = () => {
-  return async (dispatch: AppDispatch) => {
-    const fakeDBData = { name: "John", id: 547 };
-    dispatch(authSlice.actions.authenticateUser({ user: fakeDBData }));
-  };
-};
