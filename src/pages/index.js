14e97ac490f153eef3eeb41b7d8165e6ac3e89a1
Stoffel <86371768+vdwstoffel@users.noreply.github.com>
2024-08-19 19:29:21 +0200

    add home page

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '@theme/Layout';
+
+export default function Home() {
+    return (
+    <Layout>
+        <h1>Do SOMETHING ON THIS PAGE</h1>
+    </Layout>
+    )
+}
